2aae571a8f15293cff20f11f0c102beebcdaa921
Akshat Pancholi <itsakshatpancholi@gmail.com>
2021-06-12 16:53:24 -0700

    Fixed Testing

--- tests/test_pickle.py
+++ tests/test_pickle.py
@@ -1,12 +1,116 @@
-''' 
+'''
 Test pickle data
 '''
 from fast_ner.utils.entity_handling import load_entities
 
+
 def test_pickle_keys():
     data = load_entities(from_pickle=True)
-    assert(str(data.keys()) == "dict_keys(['trailer', 'director', 'actor', 'ratings_average', 'review', 'rating', 'year', 'movies', 'character', 'genre', 'plot', 'title', 'song'])")
+    expected_keys = [
+        'actor', 'character', 'director', 'genre', 'movies', 'plot', 'rating',
+        'ratings_average', 'review', 'song', 'title', 'trailer', 'year'
+    ]
+    assert(sorted(list(data.keys())) == expected_keys)
+
 
 def test_pickle_data():
     data = load_entities(from_pickle=True)
-    assert(str(data['movies']['naruto']) == "{1: 1, 'shippuden': {'blood': {'prison': {1: 1}}, 'the': {'movie': {'the': {'lost': {'tower': {1: 1}}}, 1: 1}}}, 'shippûden': {'the': {'movie': {'the': {'will': {'of': {'fire': {1: 1}}}}, 'bonds': {1: 1}}}}, 'the': {'movie': {'2': {'legend': {'of': {'the': {'stone': {'of': {'gelel': {1: 1}}}}}}}, '3': {'guardians': {'of': {'the': {'crescent': {'moon': {'kingdom': {1: 1}}}}}}}, 'ninja': {'clash': {'in': {'the': {'land': {'of': {'snow': {1: 1}}}}}}}}}}")
+    expected_entity_data = {
+        1: 1,
+        'shippuden': {
+            'blood': {
+                'prison': {
+                    1: 1
+                }
+            },
+            'the': {
+                'movie': {
+                    'the': {
+                        'lost': {
+                            'tower': {
+                                1: 1
+                            }
+                        }
+                    },
+                    1: 1
+                }
+            }
+        },
+        'shippûden': {
+            'the': {
+                'movie': {
+                    'the': {
+                        'will': {
+                            'of': {
+                                'fire': {
+                                    1: 1
+                                }
+                            }
+                        }
+                    },
+                    'bonds': {
+                        1: 1
+                    }
+                }
+            }
+        },
+        'the': {
+            'movie': {
+                '2': {
+                    'legend': {
+                        'of': {
+                            'the': {
+                                'stone': {
+                                    'of': {
+                                        'gelel': {
+                                            1: 1
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                '3': {
+                    'guardians': {
+                        'of': {
+                            'the': {
+                                'crescent': {
+                                    'moon': {
+                                        'kingdom': {
+                                            1: 1
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                'ninja': {
+                    'clash': {
+                        'in': {
+                            'the': {
+                                'land': {
+                                    'of': {
+                                        'snow': {
+                                            1: 1
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    assert(data['movies']['naruto'] == expected_entity_data)
+
+
+def main():
+    test_pickle_keys()
+    test_pickle_data()
+
+
+if __name__ == "__main__":
+    main()
--- tests/test_query.py
+++ tests/test_query.py
@@ -1,86 +1,153 @@
-from fast_ner.ner import add_new_entity, load_dict_data, load_csv_data, perform_ner
+from fast_ner.ner import add_new_entity, load_dict_data, load_csv_data
+from fast_ner.ner import perform_ner
+
 
 def test_exact_match():
     ''' Exact matching
-    Testing for single word movie titles, multi-word movie titles 
+    Testing for single word movie titles, multi-word movie titles
     and titles with special characters
     '''
-    data = load_dict_data(selected_entities=['movies']) # for exact matching
-    
-    input_string = 'I would like to watch Travel Mates 2, Circle or Marc Maron: Too Real right now.'
-    expected_output = ("{'movies': [(['travel', 'mates', '2'], 5, 8), "
-                        "(['circle'], 8, 9), (['marc', 'maron', 'too', 'real'], 10, 14)]}")
-    output = perform_ner(input_data=input_string,entity_data=data)
-    
-    assert(str(output) == expected_output)
+    data = load_dict_data(selected_entities=['movies'])  # for exact matching
+
+    input_string = ('I would like to watch Travel Mates 2, Circle or Marc '
+                    'Maron: Too Real right now.')
+    expected_output = {
+        'movies': [
+            (
+                ['travel', 'mates', '2'], 5, 8
+            ),
+            (
+                ['circle'], 8, 9
+            ),
+            (
+                ['marc', 'maron', 'too', 'real'], 10, 14
+            )
+        ]
+    }
+    output = perform_ner(input_data=input_string, entity_data=data)
+
+    assert(output == expected_output)
+
 
 def test_fuzzy_match():
     ''' Exact + Fuzzy matching
-    Testing for single word movie titles, multi-word movie titles 
+    Testing for single word movie titles, multi-word movie titles
     and titles with special characters
     '''
-    data = load_dict_data(selected_entities=['movies']) # for exact matching
-    csv_data = load_csv_data(selected_entities=['movies']) # for fuzzy matching
+    data = load_dict_data(selected_entities=['movies'])  # for exact matching
+    csv_data = load_csv_data(
+        selected_entities=['movies'])  # for fuzzy matching
 
-    input_string="I would like to watch Travel Mates 2, Circle or Marc Maron: Too Real right now."
-    expected_output = ("{'movies': [(['travel', 'mates', '2'], 5, 8), "
-                        "(['circle'], 8, 9), (['marc', 'maron', 'too', 'real'], 10, 14)], "
-                        "'fuzzy_matches': {'movies': [('Marc Maron: Too Real', 0.5456128103461935), "
-                        "('Travel Mates 2', 0.41430868766171436)]}}")
-    output = perform_ner(input_data=input_string, entity_data=data, fuzzy_matching=True, csv_data=csv_data)
-    
-    assert(str(output) == expected_output)
+    input_string = ('I would like to watch Travel Mates 2, Circle or Marc '
+                    'Maron: Too Real right now.')
+    expected_output = {
+        'movies': [
+            (
+                ['travel', 'mates', '2'], 5, 8
+            ),
+            (
+                ['circle'], 8, 9
+            ),
+            (
+                ['marc', 'maron', 'too', 'real'], 10, 14
+            )
+        ],
+        'fuzzy_matches':
+        {
+            'movies':
+            [
+                ('Marc Maron: Too Real', 0.55),
+                ('Travel Mates 2', 0.41)
+            ]
+        }
+    }
+    output = perform_ner(input_data=input_string, entity_data=data,
+                         fuzzy_matching=True, csv_data=csv_data)
+
+    # rounding fuzzy match % to 2 decimals
+    for key in output['fuzzy_matches'].keys():
+        for idx, data in enumerate(output['fuzzy_matches'][key]):
+            output['fuzzy_matches'][key][idx] = (data[0], round(data[1], 2))
+
+    assert(output == expected_output)
+
 
 def test_multiple_exact_match():
     ''' Exact matching
-    Testing for movie titles with shorter and longer names for movies in the same series
+    Testing for movie titles with shorter and longer names for movies in the
+    same series
     '''
-    data = load_dict_data(selected_entities=['movies']) # for exact matching
+    data = load_dict_data(selected_entities=['movies'])  # for exact matching
 
-    input_string='Have you watched Naruto or Naruto Shippuden : Blood Prison?'
-    expected_output = ("{'movies': [(['naruto'], 3, 4), "
-                        "(['naruto', 'shippuden', 'blood', 'prison'], 5, 9)]}")
-    output = perform_ner(input_data=input_string,entity_data=data)
-    
-    assert(str(output) == expected_output)
+    input_string = ('Have you watched Naruto or Naruto Shippuden : Blood'
+                    ' Prison?')
+    expected_output = {
+        'movies': [
+            (
+                ['naruto'], 3, 4
+            ),
+            (
+                ['naruto', 'shippuden', 'blood', 'prison'], 5, 9
+            )
+        ]
+    }
+    output = perform_ner(input_data=input_string, entity_data=data)
+
+    assert(output == expected_output)
+
 
 def test_multiple_fuzzy_match():
     ''' Exact + Fuzzy matching
-    Testing for movie titles with shorter and longer names for movies in the same series
+    Testing for movie titles with shorter and longer names for movies in the
+    same series
     '''
-    data = load_dict_data(selected_entities=['movies']) # for exact matching
-    csv_data = load_csv_data(selected_entities=['movies']) # for fuzzy matching
+    data = load_dict_data(selected_entities=['movies'])  # for exact matching
+    csv_data = load_csv_data(
+        selected_entities=['movies'])  # for fuzzy matching
 
-    input_string='Have you watched Naruto or Naruto Shippuden : Blood Prison?'
-    expected_output = ("{'movies': [(['naruto'], 3, 4), "
-                        "(['naruto', 'shippuden', 'blood', 'prison'], 5, 9)], "
-                        "'fuzzy_matches': {'movies': [('Naruto', 0.5864945177714063), "
-                        "('Naruto Shippuden : Blood Prison', 0.7566449597381272)]}}")
-    output = perform_ner(input_data=input_string, entity_data=data, fuzzy_matching=True, csv_data=csv_data)
-    
-    assert(str(output) == expected_output)
+    input_string = ('Have you watched Naruto or Naruto Shippuden : Blood'
+                    ' Prison?')
+    expected_output = {
+        'movies': [
+            (
+                ['naruto'], 3, 4
+            ),
+            (
+                ['naruto', 'shippuden', 'blood', 'prison'], 5, 9
+            )
+        ],
+        'fuzzy_matches': {
+            'movies': [
+                ('Naruto', 0.59),
+                ('Naruto Shippuden : Blood Prison', 0.76)
+            ]
+        }
+    }
+    output = perform_ner(input_data=input_string, entity_data=data,
+                         fuzzy_matching=True, csv_data=csv_data)
+
+    # rounding fuzzy match % to 2 decimals
+    for key in output['fuzzy_matches'].keys():
+        for idx, data in enumerate(output['fuzzy_matches'][key]):
+            output['fuzzy_matches'][key][idx] = (data[0], round(data[1], 2))
+
+    assert(output == expected_output)
+
 
 def reload_data():
     ''' Reconstruct dictionary
     '''
     add_new_entity()
 
-def run_all_tests():
-    ''' Load data and run all tests
-    '''
-    data = load_dict_data(selected_entities=['movies']) # for exact matching
-    csv_data = load_csv_data(selected_entities=['movies']) # for fuzzy matching
-    #data = load_dict_data() # to load all available entity data
-    #csv_data = load_csv_data() # to load all available entity data
-    
-    test_exact_match(data=data)
-    test_fuzzy_match(data=data,csv_data=csv_data)
-    test_multiple_exact_match(data=data)
-    test_multiple_fuzzy_match(data=data,csv_data=csv_data)
 
 def main():
     reload_data()
-    run_all_tests()
+
+    test_exact_match()
+    test_fuzzy_match()
+    test_multiple_exact_match()
+    test_multiple_fuzzy_match()
+
 
 if __name__ == "__main__":
     main()
