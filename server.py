65f61877c0386642e0b0122ea4ee462714ddc52d
Akshat Pancholi <itsakshatpancholi@gmail.com>
2021-06-14 14:27:23 -0700

    Added health check

--- server.py
+++ server.py
@@ -2,14 +2,20 @@ from flask import Flask, request
 from fast_ner import ner
 server = Flask(__name__)
 
+
+@server.route("/health/", methods=["GET"])
+def health_check():
+    return "success"
+
+
 @server.route("/", methods=["POST"])
 def perform_ner():
     data = request.get_json()
-    
+
     if 'input_data' not in data:
         return 'Error! Provide query data.'
     input_data = data['input_data']
-    
+
     if 'entity_data' in data:
         entity_data = data['entity_data']
     else:
@@ -26,6 +32,7 @@ def perform_ner():
         csv_data = None
 
     return ner.perform_ner(input_data, entity_data, fuzzy_matching, csv_data)
+
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0")
